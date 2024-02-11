e7d2e5a8ff353f91c218f2c4938b1e26f681619b
Kolcman <m.kolcman@yandex.ru>
2024-02-11 13:57:50 +0300

    Lesson2:task2

--- Program.cs
+++ Program.cs
@@ -13,3 +13,32 @@ else
     System.Console.WriteLine("Нет");
 }
 */
+
+
+// Task2
+
+
+Console.WriteLine("Введите кардинату X: ");
+int x = Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("Введите кардинату Y: ");
+int y = Convert.ToInt32(System.Console.ReadLine());
+if (x != 0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        System.Console.WriteLine("1 четверть");
+    }
+    if (x < 0 && y > 0)
+    {
+        System.Console.WriteLine("2 четверть");
+    }
+    if (x < 0 && y < 0)
+    {
+        System.Console.WriteLine("3 четверть");
+    }
+    if (x > 0 && y < 0)
+    {
+        System.Console.WriteLine("4 четверть");
+    }
+}
+else System.Console.WriteLine("Вы ввели нулевые кардинаты");
