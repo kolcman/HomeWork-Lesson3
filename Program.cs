54e1f581bad440c8821b9e64848953bcade9a813
Kolcman <m.kolcman@yandex.ru>
2024-02-11 14:08:24 +0300

    Lesson2:task3

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ else
 
 // Task2
 
-
+/*
 Console.WriteLine("Введите кардинату X: ");
 int x = Convert.ToInt32(System.Console.ReadLine());
 Console.WriteLine("Введите кардинату Y: ");
@@ -42,3 +42,24 @@ if (x != 0 && y != 0)
     }
 }
 else System.Console.WriteLine("Вы ввели нулевые кардинаты");
+*/
+
+
+// Task3
+
+Console.WriteLine("Введите число от 10 до 99 включительно: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+if (number >= 10 && number <= 99)
+{
+    int figure1 = number / 10;
+    int figure2 = number % 10;
+    if(figure1 > figure2){
+        System.Console.WriteLine("Первая цифра больше");
+    }
+    else if (figure1 < figure2)
+    {
+        System.Console.WriteLine("Вторая цифра больше");
+    }
+    else System.Console.WriteLine("Цифры равны");
+}
+else System.Console.WriteLine("Число вне диапозона");
