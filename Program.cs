90ff8fc246adbac35d507b528464f2e67dfb2f41
Kolcman <m.kolcman@yandex.ru>
2024-02-11 13:33:40 +0300

    Lesson2:task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task1
+
+/*
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
+*/
